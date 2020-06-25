e1b9f901ed987c9973d540779b39c2a4015db117
mamamatsumoto <matsumoto.working@gmail.com>
2020-06-25 23:26:28 +0900

    initial commit

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+    """Sing in forms"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import(LoginView, LogoutView)
+from django.views.generic import TemplateView,CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from .forms import LoginForm
+
+
+class Login(LoginView):
+    """ログインページ"""
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+
+
+class Logout(LoginRequiredMixin, LogoutView):
+    """ログアウトページ"""
+    template_name = 'accounts/login.html'
+
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/create.html'
+    success_url = reverse_lazy('quotes:top')
+    #reverse_lazy('accounts:login')
