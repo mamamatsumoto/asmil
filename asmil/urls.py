bb711ea8ef11cdcc863095411b4905378042d9dd
mamamatsumoto <matsumoto.working@gmail.com>
2020-06-27 18:04:28 +0900

    順序変更

--- asmil/urls.py
+++ asmil/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('quotes/', include('quotes.urls')),
-    path('', include('accounts.urls')),
 ]
