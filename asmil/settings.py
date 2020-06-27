57044404f68be5faa6a923f4be75a7f27880a6fc
mamamatsumoto <matsumoto.working@gmail.com>
2020-06-27 21:35:10 +0900

    modified settings py

--- asmil/settings.py
+++ asmil/settings.py
@@ -143,8 +143,6 @@ db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
