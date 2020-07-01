933a845224bdc75973ef59d87202711630603a8d
mamamatsumoto <matsumoto.working@gmail.com>
2020-07-02 03:10:17 +0900

    modified loading system

--- quotes/quotation.py
+++ quotes/quotation.py
@@ -1,8 +1,8 @@
 import openpyxl
+import pprint
 
 
-
-wb = openpyxl.load_workbook("E2004076A01見積書_税抜.xlsx")
+wb = openpyxl.load_workbook("quotes/E2004076A01見積書_税抜.xlsx")
 ws = wb["template1"]
 
 
@@ -12,57 +12,78 @@ ws = wb["template1"]
 ## Header Left
 quote_header = []
 
-for i in ws.iter_rows(min_col=3, min_row=3, max_col=3, max_row=4):
-    values_original = [cell.value for cell in i]
-    quote_header.append(values_original)
+for row in ws.iter_rows(min_col=3, max_col=78, max_row=29):
+    for cell in row:
+        cell_value = cell.value
+        if cell_value is not None:
+            quote_header.append(cell_value)
 
-for i in ws.iter_rows(min_col=65, min_row=1, max_col=65, max_row=2):
-    values_original = [cell.value for cell in i]
-    quote_header.append(values_original)
+## Quote Header Index番号確認
 
-for i in ws.iter_rows(min_col=59, min_row=6, max_col=59, max_row=7):
-    values_original = [cell.value for cell in i]
-    quote_header.append(values_original)
-
-for i in ws.iter_rows(min_col=65, min_row=8, max_col=65, max_row=10):
-    values_original = [cell.value for cell in i]
-    quote_header.append(values_original)
+#for i, j in enumerate(quote_header):
+#    print('{0}:{1}'.format(i,j))
 
 
-print(quote_header)
 
 ## 見積詳細
 
 quote_details = []
 
+
+#for row in ws.iter_rows(min_col=2, min_row=32, max_col=78, max_row=35):
+#    for cell in row:
+#        cell_value = cell.value
+#        if cell_value is not None:
+#            quote_details.append(cell_value)
+
+#print(quote_details)
+
+# Quote details Index番号確認
+
+#for i, j in enumerate(quote_details):
+#    print('{0}:{1}'.format(i,j))
+
+
+
 for i in ws.iter_rows(min_col=2, min_row=32, max_col=78, max_row=35):
     values_original = [cell.value for cell in i]
-    values_ogriginal_filtered = [j for j in values_original if j is not None]
-    quote_details.append(values_ogriginal_filtered)
+    values_original_filtered = [j for j in values_original if j is not None]
+    quote_details.append(values_original_filtered)
     #print(values_ogriginal_filtered)
     # print('----------------------')
     # print(quote_details)
     # print('----------------------')
 
+dt_col_list =[]
+
+for k in quote_details:
+    l = k[0]
+    dt_col_list.append({"works" : l})
+
+print(dt_col_list)
+    
+
+
+
 ## 二次元リスト >> 一次元リスト化
 
-dt_col1 = [k[0] for k in quote_details] # 作業名
-dt_col2 = [k[2] for k in quote_details] # 数量
-dt_col3 = [k[3] for k in quote_details] # 単位
-dt_col4 = [k[4] for k in quote_details] # 通貨
-dt_col5 = [k[5] for k in quote_details] # 単価
-dt_col6 = [k[6] for k in quote_details] # 金額
-dt_col7 = [k[7] for k in quote_details] # Ex.Rate
-dt_col8 = [k[8] for k in quote_details] # 金額(JPY)
-dt_col9 = [k[9] for k in quote_details] # 課税
+#dt_col1 = [k for k in dt_col_list] # 作業名
+#dt_col2 = [k[2] for k in quote_details] # 数量
+#dt_col3 = [k[3] for k in quote_details] # 単位
+#dt_col4 = [k[4] for k in quote_details] # 通貨
+#dt_col5 = [k[5] for k in quote_details] # 単価
+#dt_col6 = [k[6] for k in quote_details] # 金額
+#dt_col7 = [k[7] for k in quote_details] # Ex.Rate
+#dt_col8 = [k[8] for k in quote_details] # 金額(JPY)
+#dt_col9 = [k[9] for k in quote_details] # 課税
 
-# print(dt_col1)
+#print(dt_col1[0])
 
 ## 見積総合計金額計算
 
-sum_gtotal = sum(dt_col8)
+#sum_gtotal = sum(dt_col8)
 # print('----------------------')
-print("JPY " + str(sum_gtotal))
+#print("JPY " + str(sum_gtotal))
 
 ## Excel 閉じる
 wb.close()
--- quotes/views.py
+++ quotes/views.py
@@ -24,46 +24,73 @@ def quotedetail(request):
 
     quote_header = []
 
-    #宛先取得
-    for i in ws.iter_rows(min_col=3, min_row=3, max_col=3, max_row=4):
-        values_original = [cell.value for cell in i]
-        quote_header.append(values_original)
-    # 発行日/見積書NO.取得
-    for i in ws.iter_rows(min_col=65, min_row=1, max_col=65, max_row=2):
-        values_original = [cell.value for cell in i]
-        quote_header.append(values_original)
-    # 部署名取得    
-    for i in ws.iter_rows(min_col=59, min_row=6, max_col=59, max_row=7):
-        values_original = [cell.value for cell in i]
-        quote_header.append(values_original)
-    # PIC/TEL/FAX NO取得
-    for i in ws.iter_rows(min_col=65, min_row=8, max_col=65, max_row=10):
-        values_original = [cell.value for cell in i]
-        quote_header.append(values_original)
+    # Header情報取得
+    for row in ws.iter_rows(min_col=3, max_col=78, max_row=29):
+        for cell in row:
+            cell_value = cell.value
+            if cell_value is not None:
+                quote_header.append(cell_value)
+    
+    quote_details = []
 
-    #文字削除
-    delete_moji = r"[ '\[\]\u3000 ]"
+    # 見積金額詳細情報取得
+    for i in ws.iter_rows(min_col=2, min_row=32, max_col=78, max_row=35):
+        values_original = [cell.value for cell in i]
+        values_original_filtered = [j for j in values_original if j is not None]
+        quote_details.append(values_original_filtered)
 
+    dt_lists = []
 
-    issued_date = re.sub(delete_moji, "", str(quote_header[2]))
-    quote_no = re.sub(delete_moji, "", str(quote_header[3]))
-    section_1 = re.sub(delete_moji, "", str(quote_header[4]))
-    section_2 = re.sub(delete_moji, "", str(quote_header[5]))
-    pic = re.sub(delete_moji, "", str(quote_header[6]))
-    tel_no = re.sub(delete_moji, "", str(quote_header[7]))
-    fax_no = re.sub(delete_moji, "", str(quote_header[8]))
+    for k in quote_details:
+
+        dt_lists.append({
+            "works" : k[0],
+            "volume" : '{:,.3f}'.format(k[2]),
+            "unit_v" : k[3],
+            "currency" : k[4],
+            "unit_p" : '{:,.2f}'.format(k[5]),
+            "amount" : '{:,.2f}'.format(k[6]),
+            "ex_rate" : k[7],
+            "amount_j" : '{:,}'.format(k[8]),
+            "tax" : k[9],
+            })
 
     wb.close()
 
     context = {
-        'address' : quote_header[0:2],
-        'issued_date': issued_date,
-        'quote_no': quote_no,
-        'section_1': section_1,
-        'section_2': section_2,
-        'pic': pic,
-        'tel_no': tel_no,
-        'fax_no': fax_no,
+        'address' : quote_header[4:6],
+        'issued_date': quote_header[1],
+        'quote_no': quote_header[3],
+        'section_1': quote_header[6],
+        'section_2': quote_header[8],
+        'pic': quote_header[10],
+        'tel_no': quote_header[12],
+        'fax_no': quote_header[14],
+        'class1' : quote_header[19],
+        'class2' : quote_header[21],
+        'project_name' : quote_header[23],
+        'item' : quote_header[25],
+        'from' : quote_header[27],
+        'to' : quote_header[30],
+        'POL' : quote_header[33],
+        'POD' : quote_header[36],
+        'execute_from' : quote_header[39],
+        'execute_upto' : quote_header[41],
+        'validity' : quote_header[43],
+        'packages' : '{:,}'.format(quote_header[46]),
+        'max_l' : '{:,}'.format(quote_header[49]),
+        't_gw' : '{:,}'.format(quote_header[52]),
+        'max_w' : '{:,}'.format(quote_header[55]),
+        't_mm' : '{:,.3f}'.format(quote_header[58]),
+        'max_h' : '{:,}'.format(quote_header[61]),
+        't_ft' : '{:,.3f}'.format(quote_header[64]),
+        'max_wt' : '{:,}'.format(quote_header[67]),
+        't_rt' : '{:,.3f}'.format(quote_header[70]),
+        'cwt' : '{:,.1f}'.format(quote_header[73]),
+        'scope' : quote_header[75:78],
         }
+        
+    context["dt_lists"] = dt_lists
+
     return render(request, template_name, context)
 
