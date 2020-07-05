8aebd2edceb9be90386044c915040752c2ce56ac
mamamatsumoto <matsumoto.working@gmail.com>
2020-07-06 03:23:51 +0900

    loading excel file

--- quotes/quotation.py
+++ quotes/quotation.py
@@ -1,8 +1,9 @@
 import openpyxl
 import pprint
+import numpy as np
 
 
-wb = openpyxl.load_workbook("quotes/E2004076A01見積書_税抜.xlsx")
+wb = openpyxl.load_workbook("quotes/E2005008B01見積書_税抜.xlsx")
 ws = wb["template1"]
 
 
@@ -27,63 +28,122 @@ for row in ws.iter_rows(min_col=3, max_col=78, max_row=29):
 
 ## 見積詳細
 
-quote_details = []
+def is_empty(cell):
+    return cell.value is None or not str(cell.value).strip()
 
 
-#for row in ws.iter_rows(min_col=2, min_row=32, max_col=78, max_row=35):
-#    for cell in row:
-#        cell_value = cell.value
-#        if cell_value is not None:
-#            quote_details.append(cell_value)
+quote_details = [] #見積書Detail list
+quote_start = 32 #見積変動開始行
 
-#print(quote_details)
-
-# Quote details Index番号確認
-
-#for i, j in enumerate(quote_details):
-#    print('{0}:{1}'.format(i,j))
-
-
-
-for i in ws.iter_rows(min_col=2, min_row=32, max_col=78, max_row=35):
+for i in ws.iter_rows(min_col=2, min_row=quote_start, max_col=78):
+    if all(is_empty(c) for c in i):
+        break
     values_original = [cell.value for cell in i]
     values_original_filtered = [j for j in values_original if j is not None]
     quote_details.append(values_original_filtered)
-    #print(values_ogriginal_filtered)
-    # print('----------------------')
-    # print(quote_details)
-    # print('----------------------')
 
-dt_col_list =[]
+#for k, l in enumerate(quote_details):
+#    print('{0}:{1}'.format(k,l))
 
+
+### 見積項目数に応じてリスト分割
+
+quote_numbers = len(quote_details)
+
+dt_row_p1 = 29 #見積項目数上限Page.1
+dt_row_p2 = 84 #見積項目数上限Page.2
+
+
+dt_col_list = []
 for k in quote_details:
-    l = k[0]
-    dt_col_list.append({"works" : l})
+    dt_col_list.append({
+        "works":k[0],
+        "volume" : '{:,.3f}'.format(k[2]),
+        "unit_v" : k[3],
+        "currency" : k[4],
+        "unit_p" : '{:,.2f}'.format(k[5]),
+        "amount" : '{:,.2f}'.format(k[6]),
+        "ex_rate" : k[7],
+        "amount_j" : '{:,}'.format(k[8]),
+        "tax" : k[9],
+        })
+dt_list_1 = dt_col_list[0:dt_row_p1]
+dt_list_2 = dt_col_list[dt_row_p1:dt_row_p2]
+dt_list_3 = dt_col_list[dt_row_p2:quote_numbers]
 
-print(dt_col_list)
-    
 
 
+tax_total_cal = []
+duty_free_total_cal = []
+for m in quote_details:
+    if m[9] == '〇':
+        tax_total_cal.append(m[8])
+    else:
+        duty_free_total_cal.append(m[8])
 
-## 二次元リスト >> 一次元リスト化
+tax_total = sum(tax_total_cal)
+duty_free_total = sum(duty_free_total_cal)
+gtotal = tax_total + duty_free_total
 
-#dt_col1 = [k for k in dt_col_list] # 作業名
-#dt_col2 = [k[2] for k in quote_details] # 数量
-#dt_col3 = [k[3] for k in quote_details] # 単位
-#dt_col4 = [k[4] for k in quote_details] # 通貨
-#dt_col5 = [k[5] for k in quote_details] # 単価
-#dt_col6 = [k[6] for k in quote_details] # 金額
-#dt_col7 = [k[7] for k in quote_details] # Ex.Rate
-#dt_col8 = [k[8] for k in quote_details] # 金額(JPY)
-#dt_col9 = [k[9] for k in quote_details] # 課税
+grand_total = ['{:,}'.format(duty_free_total), '{:,}'.format(tax_total), '{:,}'.format(gtotal)]
 
-#print(dt_col1[0])
+grandtotal = []
+grandtotal_1 = []
+grandtotal_2 = []
+grandtotal_3 = []
 
-## 見積総合計金額計算
+for k  in grand_total:
+    grandtotal.append({
+        "duty_free_total" : k[0],
+        "tax_total" : k[1],
+        "grandtotal" : k[2],
+    })
+if quote_numbers <= dt_row_p1 - 4:
+    grandtotal_1 = grandtotal
+elif quote_numbers <= dt_row_p2 - 4:
+    grandtotal_2 = grandtotal
+else:
+    grandtotal_3 = grandtotal
 
-#sum_gtotal = sum(dt_col8)
-# print('----------------------')
-#print("JPY " + str(sum_gtotal))
+
+# Remarks
+
+remarks = []
+remarks_start = quote_start + quote_numbers + 6
+
+for row in ws.iter_rows(min_col=4, min_row=remarks_start):
+    for cell in row:
+        if all(is_empty(c) for c in row):
+            break
+        cell_value = cell.value
+        if cell_value is not None:
+            remarks.append(cell_value)
+#for k, l in enumerate(remarks):
+#    print('{0}:{1}'.format(k,l))
+
+remark1 = []
+remark2 = []
+remark3 = []
+
+
+if len(grandtotal_1) > 0:
+    remark_space1 = (124.35 - (len(dt_list_1) * 4.95 + 23.43))/3.62
+    remark1 = remarks[0:int(remark_space1)]
+    remark2 = remarks[int(remark_space1):len(remarks)]
+elif len(grandtotal_2) > 0:
+    remark2 = remarks
+else:
+    remark3 = remarks
+
+print('----------------')
+print(remark1)
+print('----------------')
+print(remark2)
+print('----------------')
+#print(remark3)
+#print('----------------')
+
+
 
 ## Excel 閉じる
 wb.close()
--- quotes/views.py
+++ quotes/views.py
@@ -14,37 +14,44 @@ def quotelist(request):
     template_name = 'quotes/quote_index.html'
     return render(request, template_name)
 
+def is_empty(cell):
+    return cell.value is None or not str(cell.value).strip()
+
 def quotedetail(request):
     template_name = 'quotes/quote_details.html'
 
     ## excel data 取得
-
-    wb = openpyxl.load_workbook("quotes/E2004076A01見積書_税抜.xlsx")
+    wb = openpyxl.load_workbook("quotes/E2005008B01見積書_税抜.xlsx")
     ws = wb["template1"]
 
+    # Header情報取得
     quote_header = []
 
-    # Header情報取得
     for row in ws.iter_rows(min_col=3, max_col=78, max_row=29):
         for cell in row:
             cell_value = cell.value
             if cell_value is not None:
                 quote_header.append(cell_value)
     
-    quote_details = []
-
     # 見積金額詳細情報取得
-    for i in ws.iter_rows(min_col=2, min_row=32, max_col=78, max_row=35):
+    quote_details = [] #見積表全体取得
+    quote_start = 32 #見積変動開始行
+    for i in ws.iter_rows(min_col=2, min_row=quote_start, max_col=78):
+        if all(is_empty(c) for c in i):
+            break
         values_original = [cell.value for cell in i]
         values_original_filtered = [j for j in values_original if j is not None]
         quote_details.append(values_original_filtered)
 
-    dt_lists = []
+    quote_numbers = len(quote_details) #見積行数取得
 
+    dt_row_p1 = 29 #見積項目数上限Page.1 // 29項目
+    dt_row_p2 = 84 #見積項目数上限Page.2 // 84項目
+
+    dt_col_list = []
     for k in quote_details:
-
-        dt_lists.append({
-            "works" : k[0],
+        dt_col_list.append({
+            "works":k[0],
             "volume" : '{:,.3f}'.format(k[2]),
             "unit_v" : k[3],
             "currency" : k[4],
@@ -53,9 +60,73 @@ def quotedetail(request):
             "ex_rate" : k[7],
             "amount_j" : '{:,}'.format(k[8]),
             "tax" : k[9],
-            })
+        })
+    dt_list_1 = dt_col_list[0:dt_row_p1]
+    dt_list_2 = dt_col_list[dt_row_p1:dt_row_p2]
+    dt_list_3 = dt_col_list[dt_row_p2:quote_numbers]
 
-    wb.close()
+
+    # 金額計算
+    tax_total_cal = []
+    duty_free_total_cal = []
+    for m in quote_details:
+        if m[9] == '〇':
+            tax_total_cal.append(m[8])
+        else:
+            duty_free_total_cal.append(m[8])
+
+    tax_total = sum(tax_total_cal)
+    duty_free_total = sum(duty_free_total_cal)
+    gtotal = tax_total + duty_free_total
+
+    grand_total = ['{:,}'.format(duty_free_total), '{:,}'.format(tax_total), '{:,}'.format(gtotal)]
+
+    grandtotal = []
+    grandtotal_1 = []
+    grandtotal_2 = []
+    grandtotal_3 = []
+
+    for k  in grand_total:
+        grandtotal.append({
+            "duty_free_total" : k[0],
+            "tax_total" : k[1],
+            "grandtotal" : k[2],
+        })
+    if quote_numbers <= dt_row_p1 - 4:
+        grandtotal_1 = grandtotal
+    elif quote_numbers <= dt_row_p2 - 4:
+        grandtotal_2 = grandtotal
+    else:
+        grandtotal_3 = grandtotal
+
+    # 付帯事項
+
+    #付帯事項開始行
+    remarks_start = quote_start + quote_numbers + 6 #備考欄開始地点
+    remarks = []
+    for row in ws.iter_rows(min_col=4, min_row=remarks_start):
+        for cell in row:
+            if all(is_empty(c) for c in row):
+                break
+            cell_value = cell.value
+            if cell_value is not None:
+                remarks.append(cell_value)
+
+    remark1 = []
+    remark2 = []
+    remark3 = []
+
+    if len(grandtotal_1) > 0:
+        remark_space1 = (124.35 - (len(dt_list_1) * 4.95 + 23.43))/3.62
+        remark1 = remarks[0:int(remark_space1)]
+        remark2 = remarks[int(remark_space1):len(remarks)]
+    elif len(grandtotal_2) > 0:
+        remark2 = remarks
+    else:
+        remark3 = remarks
+
+
+
 
     context = {
         'address' : quote_header[4:6],
@@ -88,9 +159,22 @@ def quotedetail(request):
         't_rt' : '{:,.3f}'.format(quote_header[70]),
         'cwt' : '{:,.1f}'.format(quote_header[73]),
         'scope' : quote_header[75:78],
+        'tax_total' : '{:,}'.format(tax_total),
+        'duty_free_total' : '{:,}'.format(duty_free_total),
+        'dt_list_1' : dt_list_1,
+        'dt_list_2' : dt_list_2,
+        'dt_list_3' : dt_list_3,
+        'grand_total_1' : grandtotal_1,
+        'grand_total_2' : grandtotal_2,
+        'grand_total_3' : grandtotal_3,
+        'gtotal' : '{:,}'.format(gtotal),
+        'remark_1' : remark1,
+        'remark_2' : remark2,
+        'remark_3' : remark3,
         }
         
-    context["dt_lists"] = dt_lists
+    wb.close()
+
 
     return render(request, template_name, context)
 
